a0b69f9fde7b527696929270da96f68d100f299a
admin <admin@admins-MacBook-Pro.local>
2018-04-22 21:49:31 +0100

    Share button added.Sharing only Image of view controller without buttons

--- FreedomCalc/Controller/ForthViewController.swift
+++ FreedomCalc/Controller/ForthViewController.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
+import Social
 import RealmSwift
+
 class FourthViewController: UIViewController {
+    
     var realmDB = try! Realm()
     var result: String?{
     didSet{
-    print("Result received")
-        
+        print("Result received")
     }
     }
     override func viewDidLoad() {
@@ -22,10 +24,30 @@ class FourthViewController: UIViewController {
         savingResultToDB(itemTosave: result!)
     }
     
-    
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var goBackButton: UIButton!
     @IBOutlet weak var resultTextField: UILabel!
+    
+    @IBAction func shareButtonPressed(_ sender: Any) {
+        
+        shareButton.isHidden = true
+        goBackButton.isHidden = true
+        let image = self.view.asImage()
+        shareButton.isHidden = false
+        goBackButton.isHidden = false
+        
+        let activityVC =  UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        activityVC.popoverPresentationController?.sourceView = self.view
+    
+        self.present(activityVC, animated: true, completion: nil)
+        
+
+       
+    }
     @IBAction func goBackButtonPressed(_ sender: UIButton) {
     }
+    //Function that saves result to Realm DB
     func savingResultToDB(itemTosave: String){
         let realmObject = DataModel()
         realmObject.amountOfMoneyToBeFree = itemTosave
@@ -38,5 +60,16 @@ class FourthViewController: UIViewController {
             print("ERROR\(error)")
         }
         
+    }
+}
+extension UIView {
+    
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
     }
 }
